691e0c1ce95b90434b0d55762610b69adc43e0d7
MehulBawadia <mehulbawadia@gmail.com>
2026-03-12 22:06:52 +0530

    Feat: TV series in Person details page
    Added the TV series section on the Person details page.

--- resources/views/components/person/⚡show.blade.php
+++ resources/views/components/person/⚡show.blade.php
@@ -11,6 +11,9 @@ new class extends Component
     public $heroSectionLoaded = false;
     public $heroSection = null;
 
+    public $tvSeriesLoaded = false;
+    public $tvSeries = null;
+
     public function mount($id)
     {
         $this->personId = $id;
@@ -31,6 +34,26 @@ new class extends Component
         $this->heroSection = $this->person;
         $this->heroSectionLoaded = true;
     }
+
+    public function loadTvSeries()
+    {
+        $this->tvSeriesLoaded = false;
+
+        $this->tvSeries = collect($this->person['tv_credits']['cast'])
+            ->filter(function ($tv) {
+                return ! Str::contains(cache('unwanted_tv_genres', ''), $tv['genre_ids']);
+            })
+            ->filter(function ($tv) {
+                return ! blank($tv['backdrop_path']);
+            })
+            ->sortByDesc(function ($tv) {
+                return \Carbon\Carbon::parse($tv['first_air_date'])->format('Y-m-d');
+            })
+            ->values()
+            ->toArray();
+
+        $this->tvSeriesLoaded = true;
+    }
 };
 ?>
 
@@ -43,6 +66,22 @@ new class extends Component
         <div wire:cloak wire:show="heroSectionLoaded">
             @if ($heroSection)
                 <livewire:person.hero :heroSection="$heroSection" />
+            @endif
+        </div>
+    </div>
+
+    <div wire:init="loadTvSeries" class="mt-16">
+        <h2 class="text-2xl font-bold text-white mb-6">
+            TV Shows
+        </h2>
+
+        <div wire:cloak wire:show="!tvSeriesLoaded">
+            <livewire:person.tvloader />
+        </div>
+
+        <div wire:cloak wire:show="tvSeriesLoaded">
+            @if ($tvSeries)
+                <livewire:person.tv :tvSeries="$tvSeries" />
             @endif
         </div>
     </div>
---
+++ resources/views/components/person/⚡tv.blade.php
@@ -0,0 +1,49 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    public $tv;
+
+    public function mount($tvSeries)
+    {
+        $this->tv = $tvSeries;
+    }
+};
+?>
+
+<div class="max-w-7xl mx-auto">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        @foreach($tv as $show)
+            <a href="{{ route('dramas.show', $show['id']) }}" class="relative group rounded-2xl overflow-hidden cursor-pointer">
+                <img src="https://image.tmdb.org/t/p/w500{{ $show['poster_path'] }}" class="w-full aspect-2/3 object-cover transition duration-500 ease-out group-hover:scale-110" alt="{{ $show['name'] }}" />
+
+                <div class="absolute inset-0 bg-linear-to-t from-black/90 via-black/40 to-transparent transition-opacity duration-500 opacity-80 group-hover:opacity-100"></div>
+
+                <div class="absolute inset-0 flex flex-col justify-end p-5 text-white">
+                    <div class="transform transition-all duration-500 ease-[cubic-bezier(.22,1,.36,1)] translate-y-2/3 group-hover:translate-y-0">
+                        <h3 class="font-bold text-lg leading-tight transition-all duration-300">
+                            {{ $show['name'] }}
+                        </h3>
+
+                        <div class="mt-3 text-sm text-gray-300 opacity-0 transition-all duration-500 delay-150 ease-out group-hover:opacity-100 group-hover:-translate-y-1 group-hover:scale-105 group-hover:brightness-110">
+                            <div class="flex items-center space-x-2">
+                                @if(!empty($show['first_air_date']))
+                                    <span>{{ \Carbon\Carbon::parse($show['first_air_date'])->format('Y') }}</span>
+                                @endif
+                                <span>•</span>
+                                <span>⭐ {{ number_format($show['vote_average'], 1) }}</span>
+                            </div>
+
+                            @if(!empty($show['character']))
+                                <p class="mt-2 line-clamp-3 text-gray-400">
+                                    Played as: {{ $show['character'] }}
+                                </p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </a>
+        @endforeach
+    </div>
+</div>

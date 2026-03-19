384021cc5cdff12cdecf198c8428c40b0b6f23b9
MehulBawadia <mehulbawadia@gmail.com>
2026-03-19 15:10:43 +0530

    Feat: Install vite compression for brotli
    Installed the vite compression plugin to compress the site using the
    brotli compression method.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
+        viteCompression({
+            algorithm: "brotliCompress",
+        }),
     ],
     server: {
         cors: true,
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
